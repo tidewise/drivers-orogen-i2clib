9e16587b6fd6839214c2d671af0e1544019ebf4e
Sylvain <sylvain.joyeux@tidewise.io>
2025-08-29 16:36:51 -0300

    initial commit by rock-create-orogen

---
+++ i2clibTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef i2clib_TYPES_HPP
+#define i2clib_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace i2clib {
+}
+
+#endif
+
